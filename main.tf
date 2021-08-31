49c3d1af37a501bdeeaa35b6d25478580ee0aa59
Erik Kristensen <erik@erikkristensen.com>
2021-08-31 14:51:44 -0600

    initial commit to reproduce bug 11052

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+module "vpc" {
+  source     = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=0.24.0"
+  cidr_block = "10.100.50.0/23"
+  providers = {
+    aws = aws
+  }
+}
